ff1cafce1cd0fb56d9359a5f3b7d667f06c7402d
gokulvasanthJ <gokul27mech@gmail.com>
2023-04-13 14:31:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    let fname=document.getElementById("fname").value;
+    console.log(fname);
+    let lname=document.getElementById("lname").value;
+    console.log(lname);
+    let mail=document.getElementById("mail").value;
+    console.log(mail);
+    let pass=document.getElementById("pass").value;
+    console.log(pass);
+    let num=document.getElementById("Ph").value;
+    console.log(num);
+}
